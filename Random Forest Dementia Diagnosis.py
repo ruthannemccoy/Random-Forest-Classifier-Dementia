82a702e47d438fe063a24a3f046f46b6f9dd7357
Ruthanne McCoy <ruthanne.mccoy@gmail.com>
2024-04-30 12:24:50 -0600

    Added function to pull path from OS

--- Random Forest Dementia Diagnosis.py
+++ Random Forest Dementia Diagnosis.py
@@ -3,8 +3,16 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.ensemble import RandomForestClassifier
+import os
 
-df = pd.read_csv(r"C:\Users\Ruthy\Desktop\dementia data set numerical only.csv")
+#Function to pull path from OS
+def get_file_path(filename):
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    full_path = os.path.join(base_dir, filename)
+    return full_path
+file_path = get_file_path("dementia_data_set_clean.csv")
+print(file_path)
+df = pd.read_csv(file_path)
 print(df)
 
 X = df.drop('Dementia', axis=1) #features
